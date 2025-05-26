b465e9b1cb04b2dd743db2e55d77998bb1feb4e7
Sabin Dangi <sabindangi@sabins-macbook-pro.local>
2025-05-26 12:45:49 -0500

    Simple Apache Camel Route and JUnit

---
+++ src/main/java/com/dangisabin/simple_camel_springboot/components/SimpleTimer.java
@@ -0,0 +1,16 @@
+package com.dangisabin.simple_camel_springboot.components;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleTimer extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("timer:timer?period=2000")
+                .routeId("simpleTimerId")
+                .setBody(constant("Hello World"))
+                .log(LoggingLevel.INFO,"${body}");
+    }
+}
